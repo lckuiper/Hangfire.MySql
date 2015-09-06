86139839238298fe1f5739d9d3b49fc5bcc910c3
Chris Love <chris@love.name>
2015-09-06 19:18:29 +1000

    Add SQL script to create Hangfire tables

---
+++ src/Resources/install.sql
@@ -0,0 +1,135 @@
+﻿/*
+	install.sql
+
+	Hangfire.MySql
+
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `Counter`
+-- ----------------------------
+CREATE TABLE `Counter` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Key` varchar(100) NOT NULL,
+  `Value` int(11) NOT NULL,
+  `ExpireAt` date DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `IX_Counter_Key` (`Key`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+
+
+-- ----------------------------
+-- Table structure for `DistributedLock`
+-- ----------------------------
+CREATE TABLE `DistributedLock` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Resource` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `IX_DistributedLock_Resource` (`Resource`)
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=latin1;
+
+
+-- ----------------------------
+-- Table structure for `Hash`
+-- ----------------------------
+CREATE TABLE `Hash` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Key` varchar(100) NOT NULL,
+  `Field` varchar(40) NOT NULL,
+  `Value` longtext,
+  `ExpireAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `IX_Hash_Key_Field` (`Key`,`Field`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+
+
+-- ----------------------------
+-- Table structure for `Job`
+-- ----------------------------
+CREATE TABLE `Job` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `StateId` int(11) DEFAULT NULL,
+  `StateName` varchar(20) DEFAULT NULL,
+  `InvocationData` longtext NOT NULL,
+  `Arguments` longtext NOT NULL,
+  `CreatedAt` datetime NOT NULL,
+  `ExpireAt` datetime DEFAULT NULL,
+  `FetchedAt` datetime DEFAULT NULL,
+  `StateReason` longtext,
+  `StateData` longtext,
+  PRIMARY KEY (`Id`),
+  KEY `IX_Job_StateName` (`StateName`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+
+-- ----------------------------
+-- Table structure for `JobParameter`
+-- ----------------------------
+CREATE TABLE `JobParameter` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `JobId` int(11) NOT NULL,
+  `Name` varchar(40) NOT NULL,
+  `Value` longtext,
+
+  PRIMARY KEY (`Id`),
+  KEY `FK_JobParameter_Job` (`JobId`),
+  CONSTRAINT `FK_JobParameter_Job` FOREIGN KEY (`JobId`) REFERENCES `Job` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for `JobQueue`
+-- ----------------------------
+CREATE TABLE `JobQueue` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `JobId` int(11) NOT NULL,
+  `Queue` varchar(20) NOT NULL,
+  `FetchedAt` datetime DEFAULT NULL,
+  `FetchToken` varchar(36) DEFAULT NULL,
+  
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `IX_JobQueue_FetchToken` (`FetchToken`),
+  KEY `FK_JobQueue_Job` (`JobId`),
+  CONSTRAINT `FK_JobQueue_Job` FOREIGN KEY (`JobId`) REFERENCES `Job` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for `JobState`
+-- ----------------------------
+CREATE TABLE `JobState` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `JobId` int(11) NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Reason` varchar(100) DEFAULT NULL,
+  `CreatedAt` datetime NOT NULL,
+  `Data` longtext,
+  PRIMARY KEY (`Id`),
+  KEY `FK_JobState_Job` (`JobId`),
+  CONSTRAINT `FK_JobState_Job` FOREIGN KEY (`JobId`) REFERENCES `Job` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for `Server`
+-- ----------------------------
+CREATE TABLE `Server` (
+  `Id` varchar(50) NOT NULL,
+  `Data` longtext NOT NULL,
+  `LastHeartbeat` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- ----------------------------
+-- Table structure for `Set`
+-- ----------------------------
+CREATE TABLE `Set` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Key` varchar(100) NOT NULL,
+  `Value` varchar(256) NOT NULL,
+  `Score` float NOT NULL,
+  `ExpireAt` date DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `IX_Set_Key_Value` (`Key`,`Value`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+

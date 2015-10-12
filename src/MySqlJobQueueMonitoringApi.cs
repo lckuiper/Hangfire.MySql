56ce1c1f68019170c3b04e38c649bcaa7cbb87ab
lckuiper <lckuiper@acsense.nl>
2015-10-12 06:48:02 +0200

    First (raw) implementation of some missing functions

--- src/MySqlJobQueueMonitoringApi.cs
+++ src/MySqlJobQueueMonitoringApi.cs
@@ -14,9 +14,9 @@ namespace Hangfire.MySql.src
     public class MySqlJobQueueMonitoringApi : ShortConnectingDatabaseActor, IPersistentJobQueueMonitoringApi
     {
         private readonly MySqlStorageOptions _options;
-
+        
         public MySqlJobQueueMonitoringApi(string connectionString, MySqlStorageOptions options)
-            :base(connectionString)
+            : base(connectionString)
         {
             options.Should().NotBeNull();
             _options = options;
@@ -24,22 +24,100 @@ namespace Hangfire.MySql.src
 
         public IEnumerable<string> GetQueues()
         {
-            throw new NotImplementedException();
+            // string sqlQuery = @"SELECT DISTINCT ""queue"" FROM """ + _options.SchemaName + @""".""jobqueue"";";
+            // return _connection.Query(sqlQuery).Select(x => (string)x.queue).ToList();
+
+            return UsingDatabase(db =>
+            {
+                return db.GetTable<Entities.JobQueue>()
+                    .Select(o => o.Queue).Distinct().ToList();
+            });
         }
 
         public IEnumerable<int> GetEnqueuedJobIds(string queue, int @from, int perPage)
         {
-            throw new NotImplementedException();
+            #region SQL
+            // return GetQueuedOrFetchedJobIds(queue, false, @from, perPage);
+            //string sqlQuery = string.Format(@"
+            //    SELECT j.""id"" 
+            //    FROM """ + _options.SchemaName + @""".""jobqueue"" jq
+            //    LEFT JOIN """ + _options.SchemaName + @""".""job"" j ON jq.""jobid"" = j.""id""
+            //    WHERE jq.""queue"" = @queue 
+            //    AND jq.""fetchedat"" {0}
+            //    AND j.""id"" IS NOT NULL
+            //    LIMIT @count OFFSET @start;
+            //    ", fetched ? "IS NOT NULL" : "IS NULL");    
+            #endregion SQL
+
+            return UsingDatabase(db =>
+            {
+                var query = from jq in db.GetTable<Entities.JobQueue>()
+                            join j in db.GetTable<Entities.Job>() on jq.JobId equals j.Id
+                            where (
+                                jq.Queue.Equals(queue) &&
+                                (jq.FetchedAt == NullDateTime))
+                            select j.Id;
+
+                var results = query.Skip(@from).Take(perPage);
+
+                return results.AsEnumerable();
+            });
+
         }
 
         public IEnumerable<int> GetFetchedJobIds(string queue, int @from, int perPage)
         {
-            throw new NotImplementedException();
+            #region SQL
+            // return GetQueuedOrFetchedJobIds(queue, true, @from, perPage);
+            //string sqlQuery = string.Format(@"
+            //    SELECT j.""id"" 
+            //    FROM """ + _options.SchemaName + @""".""jobqueue"" jq
+            //    LEFT JOIN """ + _options.SchemaName + @""".""job"" j ON jq.""jobid"" = j.""id""
+            //    WHERE jq.""queue"" = @queue 
+            //    AND jq.""fetchedat"" {0}
+            //    AND j.""id"" IS NOT NULL
+            //    LIMIT @count OFFSET @start;
+            //    ", fetched ? "IS NOT NULL" : "IS NULL");
+            #endregion SQL
+
+            return UsingDatabase(db =>
+            {
+                var query = from jq in db.GetTable<Entities.JobQueue>()
+                            join j in db.GetTable<Entities.Job>() on jq.JobId equals j.Id
+                            where (
+                                jq.Queue.Equals(queue) &&
+                                (jq.FetchedAt != null))
+                            select j.Id;
+
+                var results = query.Skip(@from).Take(perPage);
+
+                return results.AsEnumerable();
+            });
         }
 
         public EnqueuedAndFetchedCount GetEnqueuedAndFetchedCount(string queue)
         {
-            throw new NotImplementedException();
+            int enqueuedCount = 0;
+            int fetchedCount = 0;
+
+            UsingDatabase(db =>
+            {
+                // SELECT COUNT(*) FROM """ + _options.SchemaName + @""".""jobqueue"" WHERE ""fetchedat"" IS NULL AND ""queue"" = @queue
+                //                                                                                        -------
+                enqueuedCount = db.GetTable<Entities.JobQueue>()
+                   .Count(o => o.FetchedAt == null && o.Queue.Equals(queue));
+
+                // SELECT COUNT(*) FROM """ + _options.SchemaName + @""".""jobqueue"" WHERE ""fetchedat"" IS NOT NULL AND ""queue"" = @queue
+                //                                                                                        -----------
+                fetchedCount = db.GetTable<Entities.JobQueue>()
+                   .Count(o => o.FetchedAt != null && o.Queue.Equals(queue));
+            });
+
+            return new EnqueuedAndFetchedCount
+            {
+                EnqueuedCount = enqueuedCount,
+                FetchedCount = fetchedCount
+            };
         }
     }
 }
